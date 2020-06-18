632677c6bac1429f69538dea0c0ad39c74184d41
yourlogin <kurropike@gmail.com>
2020-06-17 19:57:03 -0600

    Nightwatch google test automation

--- tests/google.js
+++ tests/google.js
@@ -12,15 +12,19 @@ module.exports = {
     'Searching Google': browser => {
        var pineapple = (searchTerm) => {
            google
+           .pause(20000)
                 .setValue('@searchBar', [searchTerm, browser.Keys.ENTER])
                 .waitForElementVisible('@results')
                 .verify.containsText('@results', searchTerm)
                 .clearValue('@searchBar')
 
        }
-       for (let i = 0; i < 100000; i++) {
+       for (let i = 0; i < 10000; i++) {
            google
         pineapple('贅沢')
+        pineapple('ファッション')
+        pineapple('不動産')
+        pineapple('新宿')
        }
     //    pineapple('CookieMonster')
     //    pineapple('Transformers')
